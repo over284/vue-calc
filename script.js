962538569950c694cd6266576d6749d89587bb5d
over284 <104231111+over284@users.noreply.github.com>
2022-05-26 02:33:56 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let vue = new Vue({
+    el: '#app',
+    data: {
+        result: '',
+        numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+        operations: ['+','-','*','/']
+    },
+    methods: {
+        input: function(char){
+            this.result = this.result.toString();
+            this.result+=char;
+        },
+        reset: function(){
+            this.result = '';
+        },
+        calc: function(){
+            this.result = eval(this.result);
+        },
+        deleteLastChar: function(){
+            this.result = this.result.toString();
+            this.result = this.result.slice(0, -1);
+        }
+    }
+})
